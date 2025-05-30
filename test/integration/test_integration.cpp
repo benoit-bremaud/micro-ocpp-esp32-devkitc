20f6568d3fe5936391c402c7d9c8dda33262201b
benoit-bremaud <benoit.bremaud@laplateforme.io>
2025-05-30 22:51:33 +0200

    feat: Add integration and unit tests for ESP32 and BootNotification functionality

---
+++ test/integration/test_integration.cpp
@@ -0,0 +1,23 @@
+// test/integration/test_integration.cpp
+// Fichier unique pour l'exécution des tests d'intégration PlatformIO
+#include <Arduino.h>
+#include <unity.h>
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_esp32_basic_functions() {
+    TEST_ASSERT_GREATER_THAN(0, ESP.getFreeHeap());
+    TEST_ASSERT_GREATER_THAN(0, ESP.getFlashChipSize());
+}
+
+void setup() {
+    delay(2000);
+    Serial.begin(115200);
+    Serial.println("=== Tests d'Intégration ===");
+    UNITY_BEGIN();
+    RUN_TEST(test_esp32_basic_functions);
+    UNITY_END();
+}
+
+void loop() {}
---
+++ test/unit_boot_notification/test_main.cpp
@@ -0,0 +1,35 @@
+// test/unit_boot_notification/test_main.cpp
+// Tests unitaires BootNotification pour PlatformIO
+#include <unity.h>
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_boot_notification_json_structure() {
+    DynamicJsonDocument request(1024);
+    request["chargePointVendor"] = "TestVendor";
+    request["chargePointModel"] = "TestModel";
+    TEST_ASSERT_EQUAL_STRING("TestVendor", request["chargePointVendor"]);
+    TEST_ASSERT_EQUAL_STRING("TestModel", request["chargePointModel"]);
+}
+
+void test_boot_notification_required_fields() {
+    DynamicJsonDocument request(1024);
+    request["chargePointVendor"] = "TestVendor";
+    TEST_ASSERT_TRUE(request.containsKey("chargePointVendor"));
+    TEST_ASSERT_FALSE(request.containsKey("chargePointModel"));
+}
+
+void setup() {
+    delay(2000);
+    Serial.begin(115200);
+    Serial.println("=== Tests BootNotification ===");
+    UNITY_BEGIN();
+    RUN_TEST(test_boot_notification_json_structure);
+    RUN_TEST(test_boot_notification_required_fields);
+    UNITY_END();
+}
+
+void loop() {}
